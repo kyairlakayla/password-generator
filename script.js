2a3df554a19afc2481228cf8f4c848603b2b47fb
Kayla <kyairlakayla.davis@gmail.com>
2021-07-14 10:12:19 -0500

    create password conditions

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 
 // Assignment code here
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+var passwordLength;
+var includeLowercase;
+var includeUppercase;
+var includeNumbers;
+var includeSymbols;
+var userChoices;
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@$#*&%()+?/=-~'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-function generatePassword() {
-  passwordLength = prompt("Choose password length. Password must be between 8 and 128 characters!");
-  includeLowercase = confirm("Include lowercase letters?");
-  includeUppercase = confirm("Include uppercase letters?");
-  includeNumbers = confirm("Include numbers?");
-  includeSymbols = confirm("Include special characters?");
-
-
-var password = getRandomLower+ getRandomUpper + getRandomNumber + getRandomSymbol
-return password
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -40,15 +24,105 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+function generatePassword() {
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  
+  if(!passwordLength) {
+    alert("Required value");
 
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 and 128");
+    console.log("Password length " + passwordLength);
+  
+  } else { 
+    includeLowercase = confirm("Include lower case letters?");
+    includeUppercase = confirm("Include upper case letters?");
+    includeNumbers = confirm("Include numbers?");
+    includeSymbols = confirm("Include special characters?");
 
-var passwordLength;
-var includeLowercase;
-var includeUppercase;
-var includeNumbers;
-var includeSymbols;
+  };
+
+  // No answer then
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSymbols) {
+    userChoices = alert("You must choose a criteria");
+  // 4 true options
+  } else if (includeLowercase && includeUppercase && includeNumbers && includeSymbols) {
+    userChoices = lowerCase.concat(upperCase, numbers, symbols);
+    console.log(userChoices);
+  }
+  // 3 true options
+  else if (includeLowercase && includeUppercase && includeNumbers) {
+    userChoices = lowerCase.concat(upperCase, numbers);
+    console.log(userChoices);
+  }
+  else if (includeLowercase && includeUppercase && includeSymbols) {
+    userChoices = lowerCase.concat(upperCase, symbols);
+    console.log(userChoices);
+  }
+  else if (includeLowercase && includeNumbers && includeSymbols) {
+    userChoices = lowerCase.concat(numbers, symbols);
+    console.log(userChoices);
+  }
+  else if (includeUppercase && includeNumbers && includeSymbols) {
+    userChoices = upperCase.concat(numbers, symbols);
+    console.log(userChoices);
+  }
+  // 2 true options
+  else if (includeLowercase && includeUppercase) {
+    userChoices = lowerCase.concat(upperCase);
+    console.log(userChoices);
+  }
+  else if (includeLowercase && includeNumbers) {
+    userChoices = lowerCase.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (includeLowercase && includeSymbols) {
+    userChoices = lowerCase.concat(symbols);
+    console.log(userChoices);
+  }
+  else if (includeUppercase && includeNumbers) {
+    userChoices = upperCase.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (includeUppercase && includeSymbols) {
+    userChoices = upperCase.concat(symbols);
+    console.log(userChoices);
+  }
+  else if (includeNumbers && includeSymbols) {
+    userChoices = numbers.concat(symbols);
+    console.log(userChoices);
+  }
+  // 1 true option
+  else if (includeLowercase) {
+    userChoices = lowerCase;
+    console.log(userChoices);
+  }
+  else if (includeUppercase) {
+    userChoices = upperCase;
+    console.log(userChoices);
+  }
+  else if (includeNumbers) {
+    userChoices = numbers;
+    console.log(userChoices);
+  }
+  else if (includeSymbols) {
+    userChoices = symbols;
+    console.log(userChoices);
+  };
+
+  var passwordBlank = [];
+
+  // Loop for random selection
+  for (var i = 0; i < passwordLength; i++) {
+  var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)]
+  passwordBlank.push(allChoices);
+  
+}
+var password = passwordBlank.join("");
+return password;
+}; 
+ 
